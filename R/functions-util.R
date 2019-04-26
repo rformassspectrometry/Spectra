e9da4e151cc692a9b583e881c74faa6231e95485
jorainer <johannes.rainer@gmail.com>
2019-04-26 08:51:13 +0200

    Copy utils.clean and utils.enableNeighbors from MSnbase

--- R/functions-util.R
+++ R/functions-util.R
@@ -99,3 +99,53 @@
     }
     x
 }
+
+#' Removes zeros from input except the ones that in the direct neighbourhood of
+#' non-zero values.
+#'
+#' @note
+#'
+#' Copied from MSnbase
+#'
+#' @param x \code{numeric}, vector to be cleaned
+#' @param all \code{logical}, should all zeros be removed?
+#' @param na.rm \code{logical}, should NAs removed before looking for zeros?
+#' @return logical vector, \code{TRUE} for keeping the value
+#' @note The return value for \code{NA} is always \code{FALSE}.
+#' @examples
+#' x <- c(1, 0, 0, 0, 1, 1, 1, 0, 0, 1, 0, 0, 0)
+#' #      T, T, F, T, T, T, T, T, T, T, T, F, F
+#' r <- c(TRUE, TRUE, FALSE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE,
+#'        FALSE, FALSE)
+#' stopifnot(utils.clean(x) == r)
+#' @noRd
+utils.clean <- function(x, all=FALSE, na.rm=FALSE) {
+  notNA <- !is.na(x)
+  notZero <- x != 0 & notNA
+
+  if (all) {
+    notZero
+  } else if (na.rm) {
+    notNA[notNA] <- utils.enableNeighbours(notZero[notNA])
+    notNA
+  } else {
+    utils.enableNeighbours(notZero)
+  }
+}
+
+#' Switch FALSE to TRUE in the direct neighborhod of TRUE.
+#' (used in utils.clean)
+#'
+#' @param x logical
+#' @return logical
+#' @examples
+#' x <- c(TRUE, TRUE, FALSE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE,
+#'        FALSE, FALSE)
+#' r <- c(TRUE, TRUE, FALSE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE,
+#'        FALSE, FALSE)
+#' stopifnot(utils.enableNeighbours(x) == r)
+#' @noRd
+utils.enableNeighbours <- function(x) {
+  stopifnot(is.logical(x))
+  x | c(x[-1], FALSE) | c(FALSE, x[-length(x)])
+}
