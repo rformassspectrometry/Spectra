355fe13e7268267c1b0a183627eca6c8e96d4820
jorainer <johannes.rainer@gmail.com>
2020-07-03 13:25:23 +0200

    docs: fix aliases

--- R/Spectra.R
+++ R/Spectra.R
@@ -3,7 +3,7 @@ NULL
 
 #' @title The Spectra class to manage and access MS data
 #'
-#' @aliases Spectra-class [,Spectra-method compareSpectra asDataFrame
+#' @aliases Spectra-class [,Spectra-method
 #'
 #' @name Spectra
 #'
