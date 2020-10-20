85595def7e114ab3d5163c135b8d4548f238fedd
jorainer <johannes.rainer@gmail.com>
2020-10-20 16:29:11 +0200

    fix: re-add mz and intensity as core spectra variables.

--- R/MsBackendDataFrame-functions.R
+++ R/MsBackendDataFrame-functions.R
@@ -85,6 +85,8 @@ NULL
     rtime = "numeric",
     acquisitionNum = "integer",
     scanIndex = "integer",
+    mz = "NumericList",
+    intensity = "NumericList",
     dataStorage = "character",
     dataOrigin = "character",
     centroided = "logical",
