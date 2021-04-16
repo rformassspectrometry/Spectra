a0c0a6890a128609bf9dbf586fd34eec1f4eafe5
andreavicini <andrea1.vicini@mail.polimi.it>
2021-04-16 15:42:16 +0200

    improvements

--- R/function-isotope.R
+++ R/function-isotope.R
@@ -6,15 +6,19 @@
 #'
 #' @param x `matrix` with spectrum data (columns `mz` and `intensity`).
 #'
-#' @param isotopeDefinition `matrix` with isotopes definition (columns `mzd`
-#' and `maxint`). This matrix has to have rows ordered according to the column
-#' `mzd` values in order for each returned group to be ordered.
+#' @param isotopeDefinition `matrix` with isotopes definition (columns `mzd`,
+#' `min_intensity` and `max_intensity`). This matrix has to have rows ordered 
+#' according to the column `mzd` values in order for each returned group to be 
+#' ordered.
+#' 
+#' @param tolerance numeric representing the tolerance for the relaxed matching 
+#' of m/z values of peaks
 #'
 #' @param ppm numeric(1) representing a relative, value-specific
 #' parts-per-million (PPM) tolerance for the relaxed matching of m/z values of
 #' peaks
 #'
-#' @param seed_mz numeric ordered vector containing m/z values. If provided,
+#' @param seedMz numeric ordered vector containing m/z values. If provided,
 #' the function checks if there are peaks in x whose m/z match them. If so,
 #' it looks for isotope groups on this subsets of peaks.
 #'
@@ -25,22 +29,23 @@
 #' value in `x` it computes m/z of would-be isotope peaks by adding the `mzd`s
 #' in `isotopeDefinition`. Possible matches of those with the other m/z values
 #' in `x` are then searched. If any is found and the ratio between the
-#' intensities of the corresponding peaks and the `i`-th peak is smaller than a
-#' threshold (specified in the `maxint` column of isotopeDefinition`), 
-#' their indexes are grouped together. Those indexes are excluded from the set 
-#' of indexes that are searched for further groups.
+#' intensities of the corresponding peaks and the `i`-th peak is in between two
+#' given values (specified in the `min_intensity` and `max_intensity` column of 
+#' isotopeDefinition`), their indexes are grouped together. Those indexes are 
+#' excluded from the set of indexes that are searched for further groups.
 #'
 #' @examples
 #'
-.isotope_peaks <- function(x, isotopeDefinition = isotopeDefinition(), ppm = 20,
-                           seed_mz = NULL) {
+.isotope_peaks <- function(x, isotopeDefinition = isotopeDefinition(), 
+                           tolerance = 0, ppm = 20, seedMz = numeric()) {
   lst <- list()
   to_test <- rep(TRUE, nrow(x))
   to_test[which(x[, 2] <= 0)] <- FALSE
-  if (is.null(seed_mz)) idxs <- which(to_test)
-  else idxs <- na.omit(MsCoreUtils::closest(seed_mz, x[which(to_test), 1], 
-                                            tolerance = 0, ppm = ppm, 
-                                            duplicates = "closest"))
+  if (length(seedMz)) 
+    idxs <- na.omit(MsCoreUtils::closest(seedMz, x[which(to_test), 1], 
+                                         tolerance = tolerance, ppm = ppm, 
+                                         duplicates = "closest"))
+  else idxs <- which(to_test)
   for (i in idxs)
   {
     if(to_test[i])
@@ -48,8 +53,10 @@
       to_test[i] <- FALSE
       tmp <- x[i, 1] + isotopeDefinition[, 1]
       wtt <- which(to_test)
-      cls <- MsCoreUtils::closest(tmp, x[wtt, 1], tolerance = 0, ppm = ppm)
-      int_ok <- which(x[, 2][wtt[cls]] < isotopeDefinition[, 2] * x[i, 2])
+      cls <- MsCoreUtils::closest(tmp, x[wtt, 1], tolerance = tolerance, 
+                                  ppm = ppm)
+      int_ok <- .is_isotope_intensity_range(x[, 2][wtt[cls]], x[i, 2], 
+                                            isotopeDefinition)
       if (length(int_ok) > 0)
       {
         lst[[length(lst) + 1]] <- c(i, wtt[cls][int_ok])
@@ -60,6 +67,26 @@
   lst
 }
 
+
+
+#' Title Checking the intensity
+#'
+#' @param x intensity of the matching peaks. x has length equal to the number 
+#' of rows of isotopeDefinition. The i-th element of x represent the intensity 
+#' associated to the i-th mzd in isotopeDefinition if any or NA. 
+#' @param intensity  intensity of the main peak
+#' @param isotopeDefinition isotope definition data.frame
+#'
+#' @return indexes of the intensities in x that are part of a isotopic group
+#' @export
+#'
+#' @examples
+.is_isotope_intensity_range <- function(x, intensity, isotopeDefinition) {
+  which(x >= isotopeDefinition[, "min_intensity"] * intensity & 
+          x <= isotopeDefinition[, "max_intensity"] * intensity)
+}
+
+
 #' Title Isotope definition
 #'
 #' @description
@@ -67,7 +94,7 @@
 #'
 #' @param charge specifies whether single or double charged ions are expected
 #'
-#' @return `data.frame` with columns `mzd` and `maxint`
+#' @return `data.frame` with columns `mzd`, `min_intensity` and `max_intensity`
 
 # isotopeDefinition <- function(charge = c(1L, 2L)) {
 #   data.frame(mz = c(1.0033, 0.99...), intensity = ....)
