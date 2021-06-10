baf20b2924de801b9740caff9ebbc9e830da0bfb
andreavicini <andrea1.vicini@mail.polimi.it>
2021-06-10 16:31:29 +0200

    Updates to .isotope_peaks function

--- R/function-isotope.R
+++ R/function-isotope.R
@@ -6,10 +6,10 @@
 #'
 #' @param x `matrix` with spectrum data (columns `mz` and `intensity`).
 #'
-#' @param isotopeDefinition `matrix` with isotopes definition (columns `mzd`,
-#' `min_intensity` and `max_intensity`). This matrix has to have rows ordered 
-#' according to the column `mzd` values in order for each returned group to be 
-#' ordered.
+#' @param isotopeDefinition `matrix` with isotopes definition (columns 
+#' `subst_name`, `subst_degree`, `md`, `min_slope`, `max_slope`). This matrix 
+#' has to have rows ordered in such a way that the column `md` is sorted in 
+#' increasing order.
 #' 
 #' @param tolerance numeric representing the tolerance for the relaxed matching 
 #' of m/z values of peaks
@@ -20,24 +20,29 @@
 #'
 #' @param seedMz numeric ordered vector containing m/z values. If provided,
 #' the function checks if there are peaks in x whose m/z match them. If so,
-#' it looks for isotope groups on this subsets of peaks.
+#' it looks for isotope groups related to this subset of peaks.
+#' 
+#' @param charge numeric(1) representing the charge of the ionized compounds 
 #'
 #' @return list of vectors. Each vector in the returned list contains the
-#' indexes of the rows in `x` that match a isotope.
+#' indexes of the rows in `x` that match a certain isotope group found by 
+#' the function.
 #'
-#' @details The function iterates over the rows of `x`. For the `i`-th m/z
-#' value in `x` it computes m/z of would-be isotope peaks by adding the `mzd`s
-#' in `isotopeDefinition`. Possible matches of those with the other m/z values
-#' in `x` are then searched. If any is found and the ratio between the
-#' intensities of the corresponding peaks and the `i`-th peak is in between two
-#' given values (specified in the `min_intensity` and `max_intensity` column of 
-#' isotopeDefinition`), their indexes are grouped together. Those indexes are 
-#' excluded from the set of indexes that are searched for further groups.
+#' @details The function iterates over the peaks (rows) in x. Firstly, it checks 
+#' the presence of peaks whose m/z difference with respect to the current peak 
+#' matches (the matching can be relaxed via the parameters `ppm` and 
+#' `tolerance`) certain m/z differences related to mass differences of 
+#' certain substitutions in `substDefinition` and the chosen `charge`. Then, if 
+#' any such peak is found, the function checks if also their intensity is 
+#' compatible with them being part of a isotopic group and if so they are 
+#' grouped together. When some peaks are grouped together their indexes are 
+#' excluded from the set of indexes that are searched for further groups. 
 #'
 #' @examples
 #'
-.isotope_peaks <- function(x, isotopeDefinition = isotopeDefinition(), 
-                           tolerance = 0, ppm = 20, seedMz = numeric()) {
+.isotope_peaks <- function(x, substDefinition = substDefinition(), 
+                           tolerance = 0, ppm = 20, seedMz = numeric(),
+                           charge = 1) {
   lst <- list()
   to_test <- rep(TRUE, nrow(x))
   to_test[which(x[, 2] <= 0)] <- FALSE
@@ -46,17 +51,18 @@
                                          tolerance = tolerance, ppm = ppm, 
                                          duplicates = "closest"))
   else idxs <- which(to_test)
+  mzd <- substDefinition[, "md"]/charge
   for (i in idxs)
   {
     if(to_test[i])
     {
       to_test[i] <- FALSE
-      tmp <- x[i, 1] + isotopeDefinition[, 1]
+      tmp <- x[i, 1] + mzd
       wtt <- which(to_test)
       cls <- MsCoreUtils::closest(tmp, x[wtt, 1], tolerance = tolerance, 
-                                  ppm = ppm)
-      int_ok <- .is_isotope_intensity_range(x[, 2][wtt[cls]], x[i, 2], 
-                                            isotopeDefinition)
+                                  ppm = ppm, duplicates = "closest")
+      int_ok <- .is_isotope_intensity_range(x[, 2][wtt[cls]], x[i, 1] * charge,
+                                            x[i, 2], substDefinition)
       if (length(int_ok) > 0)
       {
         lst[[length(lst) + 1]] <- c(i, wtt[cls][int_ok])
@@ -68,22 +74,24 @@
 }
 
 
-
 #' Title Checking the intensity
 #'
 #' @param x intensity of the matching peaks. x has length equal to the number 
-#' of rows of isotopeDefinition. The i-th element of x represent the intensity 
-#' associated to the i-th mzd in isotopeDefinition if any or NA. 
-#' @param intensity  intensity of the main peak
-#' @param isotopeDefinition isotope definition data.frame
+#' of rows of substDefinition. The i-th element of x represent the intensity 
+#' associated to the peak whose m/z difference is associated to the i-th mzd in 
+#' substDefinition if any or NA. 
+#' @param m mass of the current (assumed monoisotopic) peak
+#' @param intensity  intensity of the current (assumed monoisotopic) peak
+#' @param substDefinition substitutions definition data.frame
 #'
 #' @return indexes of the intensities in x that are part of a isotopic group
 #' @export
 #'
 #' @examples
-.is_isotope_intensity_range <- function(x, intensity, isotopeDefinition) {
-  which(x >= isotopeDefinition[, "min_intensity"] * intensity & 
-          x <= isotopeDefinition[, "max_intensity"] * intensity)
+.is_isotope_intensity_range <- function(x, m, intensity, substDefinition) {
+  R_min <- (m * substDefinition[, "min_slope"]) ^ substDefinition[, "subst_degree"]
+  R_max <- (m * substDefinition[, "max_slope"]) ^ substDefinition[, "subst_degree"]
+  which(x >= R_min * intensity & x <= R_max * intensity)
 }
 
 
@@ -94,7 +102,7 @@
 #'
 #' @param charge specifies whether single or double charged ions are expected
 #'
-#' @return `data.frame` with columns `mzd`, `min_intensity` and `max_intensity`
+#' @return `data.frame` with columns `mzd`, `min_ratio` and `max_ratio`
 
 # isotopeDefinition <- function(charge = c(1L, 2L)) {
 #   data.frame(mz = c(1.0033, 0.99...), intensity = ....)
--- tests/testthat/test_function-isotope.R
+++ tests/testthat/test_function-isotope.R
@@ -1,14 +1,18 @@
 library(testthat)
 test_that(".isotope_peaks works", {
   
-  ints <- c(1, 2, 1, 1, 0, 1, 2, 1, 0.6)
-  x <- cbind(mz = seq_along(ints), intensity = ints)
-  isoDef <- cbind(mzd = c(1, 2, 3), maxint = c(0.7, 0.6, 0.4))
-  plot(x, type = "h")
+  ints <- c(1, 6, 3, 5, 15)
+  mz <- 1:5
+  x <- cbind(mz = mz, intensity = ints)
+  # plot(x, type = "h")
+  substDef <- cbind(md = c(1, 2), 
+                    subst_degree = c(1, 2), 
+                    min_slope = c(5, 1/2),
+                    max_slope = c(7, 1))
+  res <- .isotope_peaks(x, substDef)
+  expect_equal(res, list(c(1, 2), c(3, 5)))
   
-  res <- .isotope_peaks(x, isotopeDefinition = isoDef)
-  expect_equal(res, list(c(2, 3, 4), c(7, 8, 9)))
-  
-  res <- .isotope_peaks(x, isotopeDefinition = isoDef, seed_mz = x[c(2, 6), 1])
-  expect_equal(res, list(c(2, 3, 4)))
+  res <- .isotope_peaks(x, substDef, seedMz = x[c(3), 1])
+  expect_equal(res, list(c(3, 5)))
 })
+                                                                                                 
