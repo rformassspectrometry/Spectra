e326078c8445a2819e9fdf9fa6d75a0b9f9d0e13
lgatto <laurent.gatto@gmail.com>
2021-11-21 16:30:18 +0100

    tests: add nSequences unit test

---
+++ tests/testthat/test_nSequences.R
@@ -0,0 +1,59 @@
+spdf <- data.frame(msLevel = as.integer(rep(rep(c(1, 2), c(1, 5)), 3)),
+                   acquisitionNum = 1:18)
+spdf$precScanNum <- ifelse(spdf$msLevel == 1, NA,
+                           rep(spdf$acquisitionNum[spdf$msLevel == 1], each = 6))
+
+test_that("nSequences() works", {
+    sp <- Spectra(spdf)
+    expect_error(nSequences(sp))
+})
+
+
+test_that("nSequences() works", {
+    spdf$sequence <- ifelse(spdf$msLevel == 1, NA, "PEPTIDE")
+    sp <- Spectra(spdf)
+    ans <- nSequences(sp)
+    ## All 5 MS2 scans are identified
+    expect_identical(ans$nSequences,
+                     as.integer(rep(rep(c(5, 1), c(1, 5)), 3)))
+    ## 4 MS2 scans are identified
+    sp$sequence[which(msLevel(sp) == 1) + 1] <- NA
+    ans <- nSequences(sp)
+    expect_identical(ans$nSequences,
+                     as.integer(rep(c(4, 0, 1, 1, 1, 1), 3)))
+    ## 3 MS2 scans are identified
+    sp$sequence[which(msLevel(sp) == 1) + 2] <- NA
+    ans <- nSequences(sp)
+    expect_identical(ans$nSequences,
+                     as.integer(rep(c(3, 0, 0, 1, 1, 1), 3)))
+    ## 2 MS2 scans are identified
+    sp$sequence[which(msLevel(sp) == 1) + 3] <- NA
+    ans <- nSequences(sp)
+    expect_identical(ans$nSequences,
+                     as.integer(rep(c(2, 0, 0, 0, 1, 1), 3)))
+    ## 1 MS2 scan is identified
+    sp$sequence[which(msLevel(sp) == 1) + 4] <- NA
+    ans <- nSequences(sp)
+    expect_identical(ans$nSequences,
+                     as.integer(rep(c(1, 0, 0, 0, 0, 1), 3)))
+    ## No MS2 scans are identified
+    sp$sequence[which(msLevel(sp) == 1) + 5] <- NA
+    ans <- nSequences(sp)
+    expect_identical(ans$nSequences,
+                     as.integer(rep(c(0, 0, 0, 0, 0, 0), 3)))
+})
+
+
+test_that("nSequences() works", {
+    spdf$sequence <- ifelse(spdf$msLevel == 1, NA, "PEPTIDE")
+    sp <- Spectra(spdf)
+    ## All 5 MS2 scans are identified but they don't match the MS1
+    ## acquistion numbers
+    sp$acquisitionNum[which(msLevel(sp) == 1)] <- 101:103
+    ans <- nSequences(sp)
+    expect_identical(ans$nSequences,
+                     as.integer(rep(c(0, 1, 1, 1, 1, 1), 3)))
+    ## No sequence and no matching acquistion numbers
+    sp$sequence <- NA
+    expect_identical(ans$nSequences, rep(0L, 18))
+})
