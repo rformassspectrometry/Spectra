9d436ea23cc60a4a421c1e42fcab8dbef92c0510
Johannes Rainer <5506112+jorainer@users.noreply.github.com>
2021-12-09 08:50:27 +0100

    Update inst/test_backends/test_MsBackend/test_spectra_variables.R
    Co-authored-by: Sebastian Gibb <mail@sebastiangibb.de>

--- inst/test_backends/test_MsBackend/test_spectra_variables.R
+++ inst/test_backends/test_MsBackend/test_spectra_variables.R
@@ -113,7 +113,7 @@ test_that("isolationWindowTargetMz", {
 
 test_that("isolationWindowUpperMz", {
     res <- isolationWindowUpperMz(be)
-    expect_true(is.numeric(res))
+    expect_type(res, "numeric")
     expect_true(length(res) == length(res))
     if (!isReadOnly(be)) {
         tmp <- be
